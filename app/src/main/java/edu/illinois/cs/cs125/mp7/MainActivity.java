e83dffb2ef4060bb5109ecfbe9b6b6aaf38d4bd7
Mateusz Dzik <madzik2@illinois.edu>
2018-04-20 15:34:13 -0500

    does not work yet hold up :(

--- app/src/main/java/edu/illinois/cs/cs125/mp7/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/mp7/MainActivity.java
@@ -2,6 +2,9 @@ package edu.illinois.cs.cs125.mp7;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    ImageButton pleaseWork = (ImageButton)findViewById(R.id.map);
+
+    pleaseWork.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+    );
 }
