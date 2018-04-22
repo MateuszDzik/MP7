a014b8895dc486e65a73bdece35c4c958cf972ae
Mateusz Dzik <madzik2@illinois.edu>
2018-04-22 16:55:54 -0500

    oceaniaaaaaaaaaaaah /

--- app/src/main/java/edu/illinois/cs/cs125/mp7/Oceania.java
+++ app/src/main/java/edu/illinois/cs/cs125/mp7/Oceania.java
@@ -1,7 +1,10 @@
 package edu.illinois.cs.cs125.mp7;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Oceania extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class Oceania extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oceania);
+
+        ImageButton australia = (ImageButton) findViewById(R.id.australia);
+
+        australia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openAustralia = new Intent(Oceania.this, Australia.class);
+                Oceania.this.startActivity(openAustralia);
+            }
+        }
+
+        );
+
+        ImageButton fiji = (ImageButton) findViewById(R.id.fiji);
+
+        fiji.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openFiji = new Intent(Oceania.this, Fiji.class);
+                Oceania.this.startActivity(openFiji);
+            }
+        }
+
+        );
     }
 }
