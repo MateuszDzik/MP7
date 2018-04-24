83a610424d34b4fb17b30361c3b3bd2bde2f2454
Mateusz Dzik <madzik2@illinois.edu>
2018-04-24 10:14:41 -0500

    webview please work for eeeeeeeeeeeeeeeeegypt

--- app/src/main/java/edu/illinois/cs/cs125/mp7/Egypt.java
+++ app/src/main/java/edu/illinois/cs/cs125/mp7/Egypt.java
@@ -2,6 +2,7 @@ package edu.illinois.cs.cs125.mp7;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class Egypt extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class Egypt extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_egypt);
+
+        WebView webview = new WebView(this);
+        setContentView(webview);
+
+        webview.loadUrl("https://www.youtube.com/" +
+                "watch?v=VsCqEu6CIKg&index=83&list=PLp8n0ijllfi64xJMXoLsB4NYxNt_fgoh5&t=0s");
     }
 }
